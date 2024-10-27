6ac26dda0d14d89682ac84cbfded1bc032aca6ad
Oskar Michalski-Stemler <oskar.michalski-stemler@assecods.pl>
2024-10-27 21:20:05 +0100

    first test added

---
+++ src/test/java/core/BaseTest.java
@@ -0,0 +1,18 @@
+package core;
+
+import com.microsoft.playwright.*;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+    protected Playwright playwright;
+    protected Browser browser;
+    protected BrowserContext context;
+    protected Page page;
+    protected String url = "https://www.saucedemo.com/";
+
+    @BeforeClass
+    protected void init(){
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+    }
+}
---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class LoginPage {
+
+    private final Page page;
+    private final Locator loginLogo;
+    private final Locator userInput;
+    private final Locator passwordInput;
+    private final Locator loginInput;
+
+    public LoginPage(Page page) {
+        this.page = page;
+        this.loginLogo = page.locator("//*[@id=\"root\"]/div/div[contains(@class, 'login-logo')]");
+        this.userInput = page.locator("//*[@id=\"user-name\"]");
+        this.passwordInput = page.locator("//*[@id=\"password\"]");
+        this.loginInput = page.locator("//*[@id=\"login-button\"]");
+    }
+
+    public void checkLoginPage(String textOfLogo){
+        assertThat(loginLogo).containsText(textOfLogo);
+    }
+    public void fillLogin(String text) {userInput.fill(text);}
+    public void fillPassword(String text) {passwordInput.fill(text);}
+    public void login(String text, String password) {
+        fillLogin(text);
+        fillPassword(password);
+        loginInput.click();
+    }
+}
+
